3428141d47be50ab6a7955f9d52103b98e840cef
Mohan Nepali <nepalimohan@outlook.com>
2021-12-06 16:48:03 +0545

    Status Implemented

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -3,6 +3,7 @@ from watchlist_app.api.serializers import MovieSerializer
 from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 @api_view(['GET','POST'])
 def movie_list(request):
@@ -17,12 +18,15 @@ def movie_list(request):
             serializers.save()
             return Response(serializers.data)
         else:
-            return Response(serializers.errors)
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET','PUT','DELETE'])
 def movie_details(request, pk):
     if request.method == 'GET':
-        movies = Movie.objects.get(pk=pk)
+        try:
+            movies = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
         serializers = MovieSerializer(movies)
         return Response(serializers.data)
     
@@ -33,7 +37,7 @@ def movie_details(request, pk):
             serializers.save()
             return Response(serializers.data)
         else:
-            return Response(serializers.errors)
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
     
     if request.method == 'DELETE':
         movies = Movie.objects.get(pk=pk)
