370415c1cdf033f9e337089e25b634a226c36d36
Mohan Nepali <nepalimohan@outlook.com>
2021-12-06 16:27:58 +0545

    New movie added using POST

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -4,11 +4,20 @@ from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-@api_view()
+@api_view(['GET','POST'])
 def movie_list(request):
-    movies = Movie.objects.all()
-    serializers = MovieSerializer(movies, many=True)
-    return Response(serializers.data)
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializers = MovieSerializer(movies, many=True)
+        return Response(serializers.data)
+    
+    if request.method == 'POST':
+        serializers = MovieSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        else:
+            return Response(serializers.errors)
 
 @api_view()
 def movie_details(request, pk):
